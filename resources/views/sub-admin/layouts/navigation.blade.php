316adcc24a32a94091410cc13708c2466a84caf3
Muhammad Usman Nadeem <musmannadeem92@gmail.com>
2024-10-20 02:26:49 +0500

    sub admin logout

--- resources/views/sub-admin/layouts/navigation.blade.php
+++ resources/views/sub-admin/layouts/navigation.blade.php
@@ -86,10 +86,10 @@
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
+                <form method="POST" action="{{ route('subadmin.auth.logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
+                    <x-responsive-nav-link :href="route('subadmin.auth.logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
                         {{ __('Log Out') }}
